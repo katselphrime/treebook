0026185612763e647cfbeac731a63517f7c60c6a
Youngho Seo <katselphrime@gmail.com>
2013-07-16 02:32:52 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'e1e4b8a865150f65ecb192e113628428a14a21844c6763ec658cacda00dcb2f03e2e5f0f560bda828cf63672e59fb19217b3c83b3a45e1201244cdf026d615cc'
